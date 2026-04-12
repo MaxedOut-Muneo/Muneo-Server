d8080b754ee064a3b23efe94700ef5d9f3576e46
seoshinehyo <springssh0504@naver.com>
2026-04-12 23:52:46 +0900

    [feat] OpenAPI 설정 변경

--- src/main/java/com/example/muneoserver/global/config/swagger/SwaggerConfig.java
+++ src/main/java/com/example/muneoserver/global/config/swagger/SwaggerConfig.java
@@ -3,19 +3,29 @@ package com.example.muneoserver.global.config.swagger;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition(
         info = @Info(
                 title = "Muneo Server API 명세서",
-                description = "Muneo-Server 프로젝트의 API 명세서입니다.",
-                version = "1.1.0"
-        )
+                description = "Muneo-Server 프로젝트의 REST API 문서입니다.",
+                version = "v0.0.1"
+        ),
+        servers = {
+                @Server(url = "http://localhost:8080", description = "Local"),
+                @Server(url = "https://dev.muneo.example.com", description = "Development"),
+                @Server(url = "https://muneo.example.com", description = "Production")
+        },
+        security = {
+                @SecurityRequirement(name = SwaggerConfig.SECURITY_SCHEME_NAME)
+        }
 )
 @SecurityScheme(
-        name = "bearerAuth",
+        name = SwaggerConfig.SECURITY_SCHEME_NAME,
         type = SecuritySchemeType.HTTP,
         scheme = "bearer",
         bearerFormat = "JWT",
