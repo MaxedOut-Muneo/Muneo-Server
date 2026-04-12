8b15724b48dbbcfa212d78593d86038c329f15ec
seoshinehyo <springssh0504@naver.com>
2026-04-12 16:40:41 +0900

    [feat] 프로젝트 초기 세팅

---
+++ src/main/java/com/example/muneoserver/MuneoServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.muneoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuneoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuneoServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/muneoserver/MuneoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.muneoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuneoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
