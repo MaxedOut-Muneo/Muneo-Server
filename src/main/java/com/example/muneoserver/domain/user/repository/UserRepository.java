6ba74138c90121162759d61f43df468bec42aea9
Dongyun Kim <djskaldrjfj@naver.com>
2026-05-13 19:00:36 +0900

    add-이메일 기반 조회 추가

--- src/main/java/com/example/muneoserver/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/muneoserver/domain/user/repository/UserRepository.java
@@ -3,11 +3,18 @@ package com.example.muneoserver.domain.user.repository;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.muneoserver.domain.user.domain.AuthProvider;
 import com.example.muneoserver.domain.user.domain.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByEmail(String email);
 
+    boolean existsByEmailAndIdNot(String email, Long id);
+
     Optional<User> findByEmail(String email);
+
+    Optional<User> findByEmailAndAuthProvider(String email, AuthProvider authProvider);
+
+    Optional<User> findByAuthProviderAndProviderId(AuthProvider authProvider, String providerId);
 }
