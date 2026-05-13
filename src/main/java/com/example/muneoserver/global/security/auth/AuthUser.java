99a6cca8ef492bb16db45b791cace2820814050b
Dongyun Kim <djskaldrjfj@naver.com>
2026-05-13 19:03:20 +0900

    feat-소셜 로그인용 유저 추가

--- src/main/java/com/example/muneoserver/global/security/auth/AuthUser.java
+++ src/main/java/com/example/muneoserver/global/security/auth/AuthUser.java
@@ -1,10 +1,13 @@
 package com.example.muneoserver.global.security.auth;
 
+import com.example.muneoserver.domain.user.domain.AuthProvider;
 import com.example.muneoserver.domain.user.domain.UserRole;
 
 public record AuthUser(
         Long id,
         String email,
+        AuthProvider authProvider,
+        boolean profileCompleted,
         UserRole role
 ) {
 }
