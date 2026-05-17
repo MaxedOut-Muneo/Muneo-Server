5b4463f32930e28834be7a29d5b89b064603ce6e
Dongyun Kim <djskaldrjfj@naver.com>
2026-05-17 15:44:55 +0900

    hotfix: swagger

--- src/main/java/com/example/muneoserver/global/config/swagger/SwaggerConfig.java
+++ src/main/java/com/example/muneoserver/global/config/swagger/SwaggerConfig.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -15,11 +14,6 @@ import org.springframework.context.annotation.Configuration;
                 description = "Muneo-Server 프로젝트의 REST API 문서입니다.",
                 version = "v0.0.1"
         ),
-        servers = {
-                @Server(url = "http://localhost:8080", description = "Local"),
-                @Server(url = "https://dev.muneo.example.com", description = "Development"),
-                @Server(url = "https://muneo.example.com", description = "Production")
-        },
         security = {
                 @SecurityRequirement(name = SwaggerConfig.SECURITY_SCHEME_NAME)
         }
